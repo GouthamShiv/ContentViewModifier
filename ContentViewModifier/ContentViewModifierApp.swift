304b3744e7b2a5e6d5dad7734c74e5e578f4ffae
goutham <gouthamshine@gmail.com>
2022-07-04 13:26:27 +0530

    Initial Commit

---
+++ ContentViewModifier/ContentViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewModifierApp.swift
+//  ContentViewModifier
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ContentViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
